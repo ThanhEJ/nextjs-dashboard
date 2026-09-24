81117ea39a000b5014782a5bd737bd2d35287d9e
EVERSTRAY-273 <chico1kimthanh@gmail.com>
2026-09-24 16:32:51 +0700

    Add Suspense and fetchRevenue directly to RevenueChart

---
+++ proxy.ts
@@ -0,0 +1,10 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export const proxy = NextAuth(authConfig).auth;
+export default proxy;
+
+export const config = {
+    // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
