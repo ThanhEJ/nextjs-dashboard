3f93fe2aee4a21a8d65610d890f9849294c1bf53
EVERSTRAY-273 <chico1kimthanh@gmail.com>
2026-09-25 10:08:46 +0700

    fix: add NextAuth route handler and correct db env var

--- auth.ts
+++ auth.ts
@@ -6,7 +6,7 @@ import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_NEW_POSTGRES_URL!, { ssl: 'require' });
 
 async function getUser(email: string): Promise<User | undefined> {
     try {
@@ -18,7 +18,7 @@ async function getUser(email: string): Promise<User | undefined> {
     }
 }
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { auth, signIn, signOut, handlers } = NextAuth({
     ...authConfig,
     providers: [
         Credentials({
